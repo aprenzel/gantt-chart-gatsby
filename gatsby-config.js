e00b5797fd0d4bf68e993c7023633c83f2c2b940
aprenzel <aprenzel@hszg.de>
2021-08-17 14:05:17 +0200

    Jobs und Ressourcen werden von Airtable importiert

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,27 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Gatsby Gantt Chart",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+  
+  `gatsby-plugin-gatsby-cloud`,
+
+  {
+    resolve: `gatsby-source-airtable`,
+    options: {
+      apiKey: `keyofGe507yAlwZXv`, // may instead specify via env, see below
+      concurrency: 5, // default, see using markdown and attachments for more information
+      tables: [
+        {
+          baseId: `apprFBK5EHobJNylY`,
+          tableName: `Jobs`,    
+        },
+        {
+          baseId: `apprFBK5EHobJNylY`,
+          tableName: `Resources`,
+        }
+      ]
+    }
+  }
+
+  ],
 };
