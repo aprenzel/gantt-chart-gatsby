f8b75b6c27a94ccba376a9edfe1a11dd68df6228
aprenzel <aprenzel@hszg.de>
2021-10-11 22:22:28 +0200

    switch to simplified Gantt Chart

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import {GanttChart} from "../GanttChart_extended"
+import {GanttChart} from "../GanttChart"
 import { useState, useEffect } from 'react';
 import '../styles/index.css';
 
